85dd1b8944a41a5aa96b89e28ab960108144144c
sol <s0li.rg@gmail.com>
2012-08-31 21:51:46 +0300

    stam

--- src/org/nihul5/other/Storage.java
+++ src/org/nihul5/other/Storage.java
@@ -17,6 +17,20 @@ public interface Storage {
 	
 	List<Message> searchMessages(String[] keywords);
 	
+	/**
+	 * @param lat
+	 *            is the angular distance, in degrees, minutes, and seconds of a
+	 *            point north or south of the Equator. Lines of latitude are
+	 *            often referred to as parallels.
+	 * @param lng
+	 *            is the angular distance, in degrees, minutes, and seconds, of
+	 *            a point east or west of the Prime (Greenwich) Meridian. Lines
+	 *            of longitude are often referred to as meridians.
+	 * @param distance
+	 * @return
+	 */
+	List<Message> searchMessages(double lat, double lng, double distance);
+	
 	boolean saveEventRegistration(int eventid, String username);
 	boolean deleteEventRegistration(int eventid, String username);
 	boolean isUserRegisteredToEvent(String username, int eventid);
